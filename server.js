05e64836fcdbd34be8da6d1408a32cbedb4b9703
Andy Wallace <andymasteroffish@gmail.com>
2016-07-16 23:30:38 -0400

    commented and cleaned

--- server.js
+++ server.js
@@ -14,13 +14,15 @@ const server = express()
 
 const io = socketIO(server);
 
-var clicks = 0;
+var clicks = 0;	//how many clicks the server has recieved. This will reset if we restart the server
 
+//this function fires when a new computer connects
+//this is where we set up everything for the socket to that computer
 io.on('connection', (socket) => {
   console.log('Client connected');
+  
   io.emit('tellClicks', {num: clicks});	//give it the value when it starts
 
-  //socket.on('disconnect', () => socket.on('disconnect', () => ););
   socket.on('disconnect', disconnect);
   
   socket.on("click", function(){
@@ -36,8 +38,9 @@ setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
 
 function startup(){
+	console.log("server started");
 	console.log(`Listening on ${ PORT }`);
-	}
+}
 
 function disconnect(){
 	console.log('Client disconnected')
